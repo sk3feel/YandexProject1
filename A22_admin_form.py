6270a45138e25cf3bef12181c27e121f09d03d53
sk3feel <kraminkarim@mail.ru>
2021-10-29 02:10:24 +0300

    ui форма для администрации

---
+++ A22_admin_form.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap, QTransform
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QFileDialog
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
+import sqlite3
+
+class Admin_Form(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('A12_admin_form.ui', self)
+        self.setWindowTitle('DutyManager')
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Admin_Form()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec_())
