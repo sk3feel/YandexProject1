531077d435705172cf910be33a191050b110e1e8
sk3feel <kraminkarim@mail.ru>
2021-11-03 16:58:38 +0300

    03.11.2021 Админ ауф

--- A22_admin_form.py
+++ A22_admin_form.py
@@ -8,22 +8,22 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QFileDialog
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 from PyQt5.QtWidgets import QInputDialog
-import sqlite3
-import funsions
+
 
 from constants import *
-from function_bd import *
 from routine_functions import *
+
+from messages import *
+
+from base_db_functions import *
 
 
 class Admin_Form(QMainWindow):
     def __init__(self, login):
         super().__init__()
         uic.loadUi('A12_admin_form.ui', self)
-        self.setWindowTitle('DutyManager')
+        self.setWindowTitle(DUTY_MANAGER)
         self.login = login
-        self.con = sqlite3.connect('duty_db.sqlite')
-        self.cur = self.con.cursor()
         self.calendar.selectionChanged.connect(self.change_day_calendar)
         self.btn_make_duty.clicked.connect(self.make_duty)
         self.btn_pick_class.clicked.connect(self.pick_class)
@@ -52,8 +52,6 @@ class Admin_Form(QMainWindow):
             elif self.self_date_in_db is False:
                 self.put_class()
 
-
-
     def load_all_ledits_btns(self):
         self.date = None
         self.clas = None
@@ -63,10 +61,10 @@ class Admin_Form(QMainWindow):
         self.ledit_date.setText('---')
 
     def reload_bd(self):
-        self.date_and_classid = get_db_dutys()
+        self.date_and_classid = select_table(DUTYS, DATE, CLASS_ID)
 
     def reload_self_date_in_db(self):
-        # self.reload_bd()
+        self.reload_bd()
         if self.date is not None:
             self.self_date_in_db = is_duty_in_date(self.date, self.date_and_classid)
             return self.self_date_in_db
@@ -74,7 +72,6 @@ class Admin_Form(QMainWindow):
             return None
 
     def change_day_calendar(self):
-        # self.reload_bd()
         dateselected = self.calendar.selectedDate()
         date_in_string = str(dateselected.toPyDate()).split('-')[::-1]
         self.date = ' '.join(date_in_string)
@@ -85,14 +82,16 @@ class Admin_Form(QMainWindow):
 
         # если дата в базе данных, вывести класс в ledit, в противно случае -
         if self.self_date_in_db:
-            self.ledit_class.setText(d_get_class_title_by_date(self.date))
+            class_id = select_one_with_aspect(DUTYS, DATE, self.date, CLASS_ID)[0]
+            class_title = select_one_with_aspect(CLASSES, CLASS_ID, class_id, TITLE)[0]
+            self.ledit_class.setText(class_title)
         else:
-            self.ledit_class.setText('-')
+            self.ledit_class.setText(EMPTYNESS)
 
     def pick_class(self):
         clas, ok_pressed = QInputDialog.getItem(
-            self, "Выберите класс", "Какой класс?",
-            tuple(funsions.titles_of_classes), 0, False)
+            self, PICK_CLASS, WHICH_CLASS,
+            tuple(titles_of_classes), 0, False)
         if ok_pressed:
             self.clas = clas
             self.btn_pick_class.setText(clas)
@@ -100,29 +99,32 @@ class Admin_Form(QMainWindow):
     def discard(self):
         self.ledit_class.setText(self.clas)
         self.clas = None
-        self.btn_pick_class.setText('Выбрать класс')
+        self.btn_pick_class.setText(TO_PICK_CLASS)
 
     def change_class(self):
-        change_class_in_date(get_class_id_by_class_title(self.clas), self.date, base_date_status)
+        class_id = select_one_with_aspect(CLASSES, TITLE, self.clas, CLASS_ID)[0]
+        update_aspect(DUTYS, CLASS_ID, class_id, DATE, self.date)
         self.discard()
 
     def put_class(self):
-        put_class_in_date(self.date, get_class_id_by_class_title(self.clas), base_date_status)
+        class_id = select_one_with_aspect(CLASSES, TITLE, self.clas, CLASS_ID)[0]
+        insert_for_dutys(self.date, class_id, base_date_status)
         self.discard()
 
     # Страница 2: личные данные
     def load_info_about_user(self):
-        info_array = get_users_info_by_log(self.login)
+        info_array = select_one_with_aspect(USERS, LOGIN, self.login, *TABLES[USERS])
         self.ledit_login.setEnabled(False)
         self.ledit_surname.setEnabled(False)
         self.ledit_name.setEnabled(False)
         self.ledit_fathername.setEnabled(False)
         self.ledit_gender.setEnabled(False)
-        self.ledit_login.setText(info_array[us_inx_login])
-        self.ledit_surname.setText(info_array[us_inx_surname])
-        self.ledit_name.setText(info_array[us_inx_name])
-        self.ledit_fathername.setText(info_array[us_inx_fathername])
-        self.ledit_gender.setText(info_array[us_inx_gender])
+
+        self.ledit_login.setText(str(info_array[us_inx_login]))
+        self.ledit_surname.setText(str((info_array[us_inx_surname])))
+        self.ledit_name.setText(str(info_array[us_inx_name]))
+        self.ledit_fathername.setText(str(info_array[us_inx_fathername]))
+        self.ledit_gender.setText(str(info_array[us_inx_gender]))
 
     # Страница 3: утвержденные дежурства
 
