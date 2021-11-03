4493f8c6cfee01f9d9a2fa415e65e59b05a04705
sk3feel <kraminkarim@mail.ru>
2021-11-03 16:57:40 +0300

    03.11.2021 Учитель ауф

--- A22_teacher_form.py
+++ A22_teacher_form.py
@@ -8,57 +8,47 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QFileDialog
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QInputDialog
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
-import sqlite3
-import funsions
 
 from constants import *
 from function_bd import *
 from routine_functions import *
 
-no_duty = '-'
+from messages import *
+
+from base_db_functions import *
+
+
 
 class Teacher_Form(QMainWindow):
     def __init__(self, login):
         super().__init__()
         uic.loadUi('A12_teacher_form.ui', self)
-        self.setWindowTitle('DutyManager')
+        self.setWindowTitle(DUTY_MANAGER)
         self.login = login
-        self.con = sqlite3.connect('duty_db.sqlite')
-        self.cur = self.con.cursor()
         self.btn_1st_st.clicked.connect(self.pick_student)
         self.btn_2nd_st.clicked.connect(self.pick_student)
         self.btn_3d_st.clicked.connect(self.pick_student)
         self.btn_end.clicked.connect(self.change_date)
         self.make_array_of_studs()
-        self.load_date()
+        self.load_date_of_near_duty()
         self.load_info_about_user()
 
     # Страница 1: выбор дежурных
-    def load_date(self):
+    def load_date_of_near_duty(self):
         self.ledit_day_month.setEnabled(False)
-        self.classid = funsions.get_users_info(self.login)[funsions.user_inx_classid]
-        self.ledit_day_month.setText(funsions.get_near_day_of_duty(self.classid))
+        self.classid = select_one_with_aspect(USERS, LOGIN, self.login, CLASS_ID)[0]
+        all_duty_days = select_all_with_aspect(DUTYS, CLASS_ID, self.classid, DATE)
+        self.near_duty_day = get_near_day_of_duty(all_duty_days)
+        self.ledit_day_month.setText(self.near_duty_day)
 
     def check_duty(self):
-        if funsions.get_near_day_of_duty(self.classid) == '-':
-            self.statusBar().showMessage('Нет утвержденных дежурств')
+        if self.near_duty_day == EMPTYNESS:
+            self.statusBar().showMessage(NO_STATED_DUTYS)
             return False
         return True
 
     def make_array_of_studs(self):
         pass
-    #     clas, self.ok_pressed = QInputDialog.getItem(
-    #         self, "Выберите класс", "Какой класс?",
-    #         (
-    #             'Крамин Карим, дежурил 4 раза', '10Б', '10В', '10Г', '10Д',
-    #             '11А', '11Б', '11В', '11Г', '11Д',
-    #             '9А', '9Б', '9В', '9Г', '9Д',
-    #             '8А', '8Б', '8В', '8Г', '8Д',
-    #             '7А', '7Б', '7В', '7Г'
-    #         ), 0, False)
-    #     if self.ok_pressed:
-    #         self.clas = clas
-    #         self.btn_pick_class.setText(clas)
 
 
     def pick_student(self):
@@ -73,7 +63,7 @@ class Teacher_Form(QMainWindow):
 
     # Страница 2: личные данные
     def load_info_about_user(self):
-        info_array = get_users_info_by_log(self.login)
+        info_array = select_one_with_aspect(USERS, LOGIN, self.login, *TABLES[USERS])
         self.ledit_login.setEnabled(False)
         self.ledit_surname.setEnabled(False)
         self.ledit_name.setEnabled(False)
@@ -81,13 +71,14 @@ class Teacher_Form(QMainWindow):
         self.ledit_class.setEnabled(False)
         self.ledit_gender.setEnabled(False)
 
-        self.ledit_login.setText(info_array[us_inx_login])
-        self.ledit_surname.setText(info_array[us_inx_surname])
-        self.ledit_name.setText(info_array[us_inx_name])
-        self.ledit_fathername.setText(info_array[us_inx_fathername])
-        self.ledit_gender.setText(info_array[us_inx_gender])
-        class_title = get_class_title_by_log(self.login)
-        self.ledit_class.setText(class_title)
+        self.ledit_login.setText(str(info_array[us_inx_login]))
+        self.ledit_surname.setText(str(info_array[us_inx_surname]))
+        self.ledit_name.setText(str(info_array[us_inx_name]))
+        self.ledit_fathername.setText(str(info_array[us_inx_fathername]))
+        self.ledit_gender.setText(str(info_array[us_inx_gender]))
+        class_title = select_one_with_aspect(CLASSES, CLASS_ID, self.classid, TITLE)[0]
+        self.ledit_class.setText(str(class_title))
+
 
 
 def except_hook(cls, exception, traceback):
