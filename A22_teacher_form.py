11987404391ef20cdf9525c38c6578a1fdcb22af
sk3feel <kraminkarim@mail.ru>
2021-11-03 21:33:25 +0300

    Реализовано большинство функционала учителся

--- A22_teacher_form.py
+++ A22_teacher_form.py
@@ -10,13 +10,21 @@ from PyQt5.QtWidgets import QInputDialog
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 
 from constants import *
-from function_bd import *
 from routine_functions import *
-
 from messages import *
-
 from base_db_functions import *
 
+login_inx = 3
+login_inx_in_line = -1
+served_inx = 2
+
+one_studens = 1
+two_studens = 2
+three_studens = 3
+
+first_st = 'first'
+sec_st = 'second'
+third_st = 'third'
 
 
 class Teacher_Form(QMainWindow):
@@ -25,41 +33,121 @@ class Teacher_Form(QMainWindow):
         uic.loadUi('A12_teacher_form.ui', self)
         self.setWindowTitle(DUTY_MANAGER)
         self.login = login
-        self.btn_1st_st.clicked.connect(self.pick_student)
-        self.btn_2nd_st.clicked.connect(self.pick_student)
-        self.btn_3d_st.clicked.connect(self.pick_student)
+        self.duty_positions = {
+            first_st: '',
+            sec_st: '',
+            third_st: ''
+        }
+        self.btn_1_st.clicked.connect(self.pick_student)
+        self.btn_2_st.clicked.connect(self.pick_student)
+        self.btn_3_st.clicked.connect(self.pick_student)
+        self.btn_accept.clicked.connect(self.accept)
         self.btn_end.clicked.connect(self.change_date)
-        self.make_array_of_studs()
         self.load_date_of_near_duty()
+        self.get_arr_of_want_studs()
+        self.load_free_ledits()
+        self.show_act_students()
         self.load_info_about_user()
 
     # Страница 1: выбор дежурных
     def load_date_of_near_duty(self):
         self.ledit_day_month.setEnabled(False)
         self.classid = select_one_with_aspect(USERS, LOGIN, self.login, CLASS_ID)[0]
-        all_duty_days = select_all_with_aspect(DUTYS, CLASS_ID, self.classid, DATE)
+        all_duty_days = select_all_with_two_aspects \
+            (DUTYS, CLASS_ID, self.classid, PASSED, base_date_status, DATE)
         self.near_duty_day = get_near_day_of_duty(all_duty_days)
         self.ledit_day_month.setText(self.near_duty_day)
 
-    def check_duty(self):
+    def check_duty_on_empty(self):
         if self.near_duty_day == EMPTYNESS:
             self.statusBar().showMessage(NO_STATED_DUTYS)
             return False
         return True
 
-    def make_array_of_studs(self):
-        pass
+    def get_arr_of_want_studs(self):
+        array_of_students = select_all_with_two_aspects(
+            USERS, CLASS_ID, self.classid, STATUS, int_student_cod,
+            SURNAME, NAME, SERVED, DESIRE_ST, LOGIN)
+        array_of_students = sort_studends_by_dutys(array_of_students)
+        array_wanna_studs = get_students_lines(array_of_students)
+        self.array_wanna_studs = array_wanna_studs
 
+    def load_free_ledits(self):
+        self.ledit_1st.setEnabled(False)
+        self.ledit_3st.setEnabled(False)
+        self.ledit_2st.setEnabled(False)
+        self.ledit_1st.setText('-')
+        self.ledit_3st.setText('-')
+        self.ledit_2st.setText('-')
+
+    def show_act_students(self):
+        act_students = select_all_with_three_aspects(USERS, CLASS_ID, self.classid,
+                                                     ACT, not_base_act,
+                                                     STATUS, int_student_cod,
+                                                     SURNAME, NAME, SERVED, DESIRE_ST, LOGIN)
+        line_act_students = get_students_lines(act_students)
+        n_studens = len(act_students)
+        if n_studens >= one_studens:
+            self.duty_positions[first_st] = act_students[0][login_inx]
+            self.ledit_1st.setText(line_act_students[0])
+        if n_studens >= two_studens:
+            self.duty_duty_positions[sec_st] = act_students[1][login_inx]
+            self.ledit_2st.setText(line_act_students[1])
+        if n_studens == three_studens:
+            self.duty_duty_positions[third_st] = act_students[2][login_inx]
+            self.ledit_3st.setText(line_act_students[2])
 
     def pick_student(self):
-        if self.check_duty():
-            pass
+        line, ok_pressed = QInputDialog.getItem(
+            self, PICK_STUDENT, WHICH_CLASS,
+            tuple(self.array_wanna_studs), 0, False)
+        if ok_pressed:
+            login = line.split()[login_inx_in_line]
+            if self.sender() == self.btn_1_st:
+                self.ledit_1st.setText(line)
+                self.duty_positions[first_st] = login
 
+            elif self.sender() == self.btn_2_st:
+                self.ledit_2st.setText(line)
+                self.duty_positions[sec_st] = login
+
+            elif self.sender() == self.btn_3_st:
+                self.ledit_3st.setText(line)
+                self.duty_positions[third_st] = login
+
+    def accept(self):
+        update_aspect(USERS, ACT, base_act, LOGIN, self.login)
 
     def change_date(self):
-        if self.check_duty():
-            pass
+        if self.check_duty_on_empty():
+            d = self.duty_positions
+            for key in d:
+                login = d[key]
+                if login != '':
+                    discard_desire = update_aspect(USERS, DESIRE_ST, base_desireSt, LOGIN, login)
+                    self.updates_and_discards(login)
+            self.updates_and_discards(self.login)
+            self.pass_day()
+            self.discard_positions()
+            self.load_date_of_near_duty()
+            self.get_arr_of_want_studs()
 
+    # Сброс параметра act, served(количество дежурств) +=1
+    def updates_and_discards(self, login):
+        last_served = select_one_with_aspect(USERS, LOGIN, login, SERVED)[0]
+        update_served = update_aspect(USERS, SERVED, last_served + 1, LOGIN, login)
+        update_act = update_aspect(USERS, ACT, base_act, LOGIN, login)
+
+    def discard_positions(self):
+        self.duty_positions = {
+            first_st: '',
+            sec_st: '',
+            third_st: ''
+        }
+        self.load_free_ledits()
+
+    def pass_day(self):
+        update_aspect(DUTYS, PASSED, not_base_date_status, DATE, self.near_duty_day)
 
     # Страница 2: личные данные
     def load_info_about_user(self):
@@ -78,7 +166,6 @@ class Teacher_Form(QMainWindow):
         self.ledit_gender.setText(str(info_array[us_inx_gender]))
         class_title = select_one_with_aspect(CLASSES, CLASS_ID, self.classid, TITLE)[0]
         self.ledit_class.setText(str(class_title))
-
 
 
 def except_hook(cls, exception, traceback):
