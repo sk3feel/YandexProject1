9e098269f93390b3ec495397583c1767889be08c
sk3feel <kraminkarim@mail.ru>
2021-11-03 16:57:07 +0300

    03.11.2021 Ученик ауф

--- A22_student_form.py
+++ A22_student_form.py
@@ -9,10 +9,11 @@ from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 
 from constants import *
-from function_bd import *
 from routine_functions import *
 
 from messages import *
+
+from base_db_functions import *
 
 
 class Student_Form(QMainWindow):
@@ -29,17 +30,19 @@ class Student_Form(QMainWindow):
     # Страница 1: подача заявки
     def load_date_of_near_duty(self):
         self.ledit_day_month.setEnabled(False)
-        classid = get_class_id_by_login(self.login)
-        self.ledit_day_month.setText(get_near_day_of_duty(classid))
+        self.classid = select_one_with_aspect(USERS, LOGIN, self.login, CLASS_ID)[0]
+        all_duty_days = select_all_with_aspect(DUTYS, CLASS_ID, self.classid, DATE)
+        near_duty_day = get_near_day_of_duty(all_duty_days)
+        self.ledit_day_month.setText(near_duty_day)
 
     def make_a_bid(self):
-        desire_true_by_login(self.login)
+        update_aspect(USERS, DESIRE_ST, bid_desireSt, LOGIN, self.login)
 
     def load_results(self):
         self.ledit_res.setEnabled(False)
-        classid = get_class_id_by_login(self.login)
-        teacher_act = is_teacher_decided(classid)
-        student_act = get_users_info_by_log(self.login)[us_inx_act]
+        teachers_login = select_one_with_aspect(CLASSES, CLASS_ID, self.classid, LOGIN_TEACHER)[0]
+        teacher_act = select_one_with_aspect(USERS, LOGIN, teachers_login, ACT)[0]
+        student_act = select_one_with_aspect(USERS, LOGIN, self.login, ACT)[0]
 
         if teacher_act and student_act:
             self.ledit_res.setText(IS_ON_DUTY)
@@ -50,7 +53,7 @@ class Student_Form(QMainWindow):
 
     # Страница 2: личные данные
     def load_info_about_user(self):
-        info_array = get_users_info_by_log(self.login)
+        info_array = select_one_with_aspect(USERS, LOGIN, self.login, *TABLES[USERS])
         self.ledit_login.setEnabled(False)
         self.ledit_surname.setEnabled(False)
         self.ledit_name.setEnabled(False)
@@ -58,13 +61,13 @@ class Student_Form(QMainWindow):
         self.ledit_class.setEnabled(False)
         self.ledit_gender.setEnabled(False)
 
-        self.ledit_login.setText(info_array[us_inx_login])
-        self.ledit_surname.setText(info_array[us_inx_surname])
-        self.ledit_name.setText(info_array[us_inx_name])
-        self.ledit_fathername.setText(info_array[us_inx_fathername])
-        self.ledit_gender.setText(info_array[us_inx_gender])
-        class_title = get_class_title_by_log(self.login)
-        self.ledit_class.setText(class_title)
+        self.ledit_login.setText(str(info_array[us_inx_login]))
+        self.ledit_surname.setText(str(info_array[us_inx_surname]))
+        self.ledit_name.setText(str(info_array[us_inx_name]))
+        self.ledit_fathername.setText(str(info_array[us_inx_fathername]))
+        self.ledit_gender.setText(str(info_array[us_inx_gender]))
+        class_title = select_one_with_aspect(CLASSES, CLASS_ID, self.classid, TITLE)[0]
+        self.ledit_class.setText(str(class_title))
 
 
 def except_hook(cls, exception, traceback):
