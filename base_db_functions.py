63d95c9e5a97498a8034c578fd0a116c8120bec8
sk3feel <kraminkarim@mail.ru>
2021-11-03 21:35:47 +0300

    добавлены новые функции для учителя

--- base_db_functions.py
+++ base_db_functions.py
@@ -20,6 +20,17 @@ def select_all_with_aspect(table_name, field, field_value, *fields):
     return cur.execute(value, (field_value,)).fetchall()
 
 
+def select_all_with_two_aspects(table_name, field1, field_value1, field2, field_value2, *fields):
+    value = f"""SELECT {', '.join(fields)} FROM {table_name} WHERE {field1}=? AND {field2}=?"""
+    return cur.execute(value, (field_value1, field_value2,)).fetchall()
+
+
+def select_all_with_three_aspects(table_name, field1, field_value1,
+                                  field2, field_value2, field3, field_value3, *fields):
+    value = f"""SELECT {', '.join(fields)} FROM {table_name} WHERE {field1}=? AND {field2}=? AND {field3}=?"""
+    return cur.execute(value, (field_value1, field_value2, field_value3,)).fetchall()
+
+
 def insert(table_name, *values):
     parametrs = values[:int(len(values))]
     value_parametrs = values[int(len(values)):]
